6b1541240bb1851a6da2845d197d4a2c1dedbe35
lillar <33026754+lillar@users.noreply.github.com>
2026-03-06 16:17:37 +0100

    Fix grid layout

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -2,7 +2,7 @@ import { PokemonCard } from "./PokemonCard"
 
 export function PokemonList( { pokemons } ) {
     return(
-        <div className="PokemonList">
+        <div className="pokemon-list">
         {pokemons.map((p) => (
             <PokemonCard key={p.id} pokemon={p}/>
         ))}
