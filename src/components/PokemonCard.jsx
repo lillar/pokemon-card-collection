e0ee232383cbda7b5b30c4e3c677e5f01fd8c55e
lillar <33026754+lillar@users.noreply.github.com>
2026-03-06 16:24:37 +0100

    Add img to pokemon-card

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,6 +1,7 @@
 export function PokemonCard( { pokemon } ) {
     return (
         <div className="pokemon-card">
+            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} alt={pokemon.name}/>
             <h2>{pokemon.name}</h2>
             <p>Type: {pokemon.type}</p>
             <p>HP: {pokemon.hp}</p>
